9fcc1c69b6ef6fd15479c3e7456934bf2b694413
tharundusa <87140369+tharundusa@users.noreply.github.com>
2021-08-04 19:09:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+alert("hello");
+var sm=document.getElementById("success message");
+var div=document.getElementById("div");
+sm.addEventListener("click",function(){
+  div.style.display='block';
+});
